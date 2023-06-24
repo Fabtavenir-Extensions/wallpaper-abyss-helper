d82f307d14243e70ebd478c0548cb85f968028d1
Flynn <ppstream123@126.com>
2023-06-24 16:11:46 +0800

    chore: Init

---
+++ src/popup/index.tsx
@@ -0,0 +1,17 @@
+
+// import * as tailwindCSSBase from "~base.css"
+// import * as tailwindStyle from "~style.css"
+// import * as popupStyle from "./index.css"
+import "../style.css"
+import "../base.css"
+import "./index.css"
+
+function IndexPopup() {
+  return (
+    <div className="text-red bg-blue-500 flex items-center justify-center h-16">
+			<h2>Wallpaper Abyss Helper: Setting</h2>
+    </div>
+  )
+}
+
+export default IndexPopup
