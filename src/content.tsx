32f4d561fb5401ca73d2881d3b7708692f9db240
Flynn <ppstream123@126.com>
2023-06-29 18:37:25 +0800

    feat: store user setting persistently

--- src/content.tsx
+++ src/content.tsx
@@ -1,31 +1,25 @@
 import cssText from "data-text:~style.css"
 import type { PlasmoCSConfig } from "plasmo" 
-import { CountButton } from "~features/count-button"
-import { settingAtom } from "~state"
-import { useAtom, getDefaultStore} from "jotai"
-import store from "~store"
+import {useStorage} from '@plasmohq/storage/hook'
 import "~base.css"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.plasmo.com/*"]
 }
-
 export const getStyle = () => {
   const style = document.createElement("style")
   style.textContent = cssText
   return style
 }
-
-console.log('defaultStore', store)
-
 const PlasmoOverlay = () => {
-	const [setting] = useAtom(settingAtom)	// store.set(33333)
+	const [width] = useStorage<number>('width')
+	const [height] = useStorage<number>('height')
+	const [adaption] = useStorage<string>('adaption')
   return (
     <div className="z-50 flex fixed top-[800px] right-8">
-			<p>width: {setting.width}</p>
-			<p>height: {setting.height}</p>
-			<p>adaption: {setting.adaption}</p>
-      <CountButton />
+			<p>width: {width}</p>
+			<p>height: {height}</p>
+			<p>adaption: {adaption}</p>
     </div>
   )
 }
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,65 +1,55 @@
+import {useStorage} from '@plasmohq/storage/hook'
+import { Storage } from '@plasmohq/storage'
+import {useEffect, useState} from 'react'
+import Setting from '~types/Setting'
 import "~base.css"
 import "~style.css"
 import "./index.css"
-import { useAtom } from "jotai"
-import {settingAtom} from "~state"
+
+
 function IndexPopup() {
-	const [setting, setSetting] = useAtom(settingAtom)
-	const updateWidth = (width)=>{
-		console.log('width', width)
-		setSetting((prev)=>({
-			...prev,
-			setting:{
-				...prev,
-				width
-			}
-		}))
+	const [width, setWidth] = useStorage<number>('width')
+	const [height, setHeight] = useStorage<number>('height')
+	const [adaption, setAdaption] = useStorage<string>('adaption')
+	const [setting, setSetting] = useState<Setting>({
+		width: 1920,
+		height: 1080,
+		adaption: "cropped"
+	})
+
+	useEffect(()=>{
+		if(!width || !height || !adaption) return
+		setSetting({
+			width: width,
+			height: height,
+			adaption: adaption 
+		})
+	},[width, height, adaption])
+	const save = ()=>{
+		setWidth(setting.width)
+		setHeight(setting.height)
+		setAdaption(setting.adaption)
 	}
-	const updateHeight = (height)=>{
-		setSetting((prev)=>({
-			...prev,
-			setting:{
-				...prev,
-				height
-			}
-		}))
-	}
-	const updateAdaption = (adaption)=>{
-		setSetting((prev)=>({
-			...prev,
-			setting:{
-				...prev,
-				adaption
-			}
-		}))
-	}
-	// Consider refactor it elegantly with ImmerJS!
   return (
     <div className="flex gap-y-2 flex-col p-5 items-start w-[300px] h-[220px]">
 			<h2 className="text-lg font-semibold">Wallpaper Abyss Helper Setting</h2>
 			<p><span>Adaption:</span>
-				<select value={setting.adaption} onChange={(e)=>setSetting({
-					width: setting.width,
-					height: setting.height,
-					adaption: e.target.value
-			})}>	
+				<select value={setting.adaption} onChange={
+					(e)=>setSetting({...setting, adaption:e.target.value})
+					}>	
 					<option value="cropped">Crop</option>
 					<option value="stretched">Stretch</option>
 				</select>
 			</p>
-			<p><span>Width:</span> <input type="number" value={setting.width} onChange={(e)=>updateWidth(e.target.value)}/>px</p>
+			<p><span>Width:</span> <input type="text" value={setting.width} onChange={(e)=>setSetting({...setting, width: Number(e.target.value)})}/>px</p>
 			<p><span>Height:</span> <input type="text" value={setting.height} 
-			onChange={(e)=>updateHeight(e.target.value)}/>px</p>
+			onChange={(e)=>setSetting({...setting, height:Number(e.target.value)})}/>px</p>
+		
 			<p><button className="btn px-3 py-2 text-white bg-blue-500 hover:bg-blue-300 rounded"
 			onClick={
-				()=>{
-					setSetting({
-						width: 1000,
-						height: 2000,
-						adaption: "hello"
-				})
-				}
-			}>Save</button></p>
+				save
+			}>Save</button>
+			</p>
     </div>
   )
 }
